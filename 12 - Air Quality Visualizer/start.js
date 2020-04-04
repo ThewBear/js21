aadb75dc05ff79baea5a57a681ec700503f66ee8
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-04-04 23:46:46 +0700

    Working on 12

--- 12 - Air Quality Visualizer/start.js
+++ 12 - Air Quality Visualizer/start.js
@@ -1,4 +1,40 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const cityElem = document.querySelector(".city");
+  const stateCountryElem = document.querySelector(".state-country");
+  const aqiElem = document.querySelector(".aqi > h1");
+  const temperatureElem = document.querySelector(".temperature");
+  const humidityElem = document.querySelector(".humidity");
+  const windElem = document.querySelector(".wind");
 
+  const city = "Bangkok";
+  const state = city;
+  const country = "Thailand";
+  const aqi = Math.floor(Math.random() * 100 + 30);
+  const temperature = Math.floor(Math.random() * 25 + 15);
+  const humidity = Math.floor(Math.random() * 50 + 35);
+  const wind = Math.floor(Math.random() * 25 + 5);
+
+  cityElem.innerText = city;
+  stateCountryElem.innerText = `${state}, ${country}`;
+  aqiElem.innerText = aqi;
+  temperatureElem.innerText = `Temp: ${temperature}°C`;
+  humidityElem.innerText = `Humidity: ${humidity}%`;
+  windElem.innerText = `Wind: ${wind} m/s`;
+
+  if (aqi <= 50) {
+    document.documentElement.style.setProperty(
+      "--current-aqi-color",
+      "var(--good-aqi-color)"
+    );
+  } else if (aqi <= 100) {
+    document.documentElement.style.setProperty(
+      "--current-aqi-color",
+      "var(--medium-aqi-color)"
+    );
+  } else {
+    document.documentElement.style.setProperty(
+      "--current-aqi-color",
+      "var(--bad-aqi-color)"
+    );
+  }
 })();
