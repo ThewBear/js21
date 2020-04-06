1c1fbaf8d60cab913a871a9b3a48e0ba72dc0039
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-04-06 23:04:12 +0700

    Working on 17

--- 17 - Speech to Text/start.js
+++ 17 - Speech to Text/start.js
@@ -1,3 +1,38 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const btnElem = document.querySelector(".control");
+
+  recognition.addEventListener("result", (event) => {
+    const textElem = document.querySelector(".text");
+    const { transcript } = event.results[0][0];
+    textElem.innerText += " " + transcript;
+  });
+  recognition.addEventListener("end", () => {
+    if (!btnElem.classList.contains("record")) {
+      recognition.start();
+    }
+  });
+
+  recognition.addEventListener("soundstart", (event) => {
+    btnElem.classList.add("hearing");
+  });
+
+  recognition.addEventListener("soundend", (event) => {
+    btnElem.classList.remove("hearing");
+  });
+
+  btnElem.addEventListener("click", () => {
+    if (btnElem.classList.contains("record")) {
+      recognition.start();
+      btnElem.classList.remove("record");
+      btnElem.classList.add("pause");
+    } else {
+      recognition.stop();
+      btnElem.classList.remove("pause");
+      btnElem.classList.add("record");
+    }
+  });
 })();
